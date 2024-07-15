aaee6e45daf4a6cc4d8aa0d08d2d14fbc646d4d0
mateo <mateo1559@gmail.com>
2024-07-16 01:35:55 +0200

    refactor: Resolver to use class method instead instance method

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,14 +1,9 @@
 module OpenWeatherMap
   # Resolver class to resolve city id
   class Resolver
-    def city_id(city_name)
+    def self.city_id(city_name)
+      city_ids = JSON.parse(File.read(File.expand_path('city_ids.json', __dir__)))
       (city_ids.find { |city| city['name'] == city_name })&.fetch('id')
-    end
-
-    private
-
-    def city_ids
-      JSON.parse(File.read(File.expand_path('city_ids.json', __dir__)))
     end
   end
 end
--- spec/domains/open_weather_map/resolver_spec.rb
+++ spec/domains/open_weather_map/resolver_spec.rb
@@ -4,18 +4,15 @@ require 'open_weather_map/resolver'
 RSpec.describe OpenWeatherMap::Resolver do
   describe '#city_id' do
     it 'returns the correct id for a known city' do
-      resolver = OpenWeatherMap::Resolver.new
-      expect(resolver.city_id('Zagreb')).to eq(3_186_886)
+      expect(OpenWeatherMap::Resolver.city_id('Zagreb')).to eq(3_186_886)
     end
 
     it 'returns nil for missing diacritics' do
-      resolver = OpenWeatherMap::Resolver.new
-      expect(resolver.city_id('Varazdin')).to be_nil
+      expect(OpenWeatherMap::Resolver.city_id('Varazdin')).to be_nil
     end
 
     it 'returns nil when called with an unknown city name' do
-      resolver = OpenWeatherMap::Resolver.new
-      expect(resolver.city_id('Some other city')).to be_nil
+      expect(OpenWeatherMap::Resolver.city_id('Some other city')).to be_nil
     end
   end
 end
