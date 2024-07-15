0a095a675bc6caf138061d89a39eb87413f8de26
mateo <mateo1559@gmail.com>
2024-07-16 00:35:59 +0200

    fix: Removed duplicated OpenWeatherMap Resolver clas

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -12,18 +12,3 @@ module OpenWeatherMap
     end
   end
 end
-
-module OpenWeatherMap
-  # Resolver class to resolve city id
-  class Resolver
-    def city_id(city_name)
-      (city_ids.find { |city| city['name'] == city_name })&.fetch('id')
-    end
-
-    private
-
-    def city_ids
-      JSON.parse(File.read(File.expand_path('city_ids.json', __dir__)))
-    end
-  end
-end
