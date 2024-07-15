56cff6639eb01ec42d3453a7439a29c11c0cb1b6
mateo <mateo1559@gmail.com>
2024-07-16 00:43:18 +0200

    refactor: resolver_spec cleanup

--- spec/domains/open_weather_map/resolver_spec.rb
+++ spec/domains/open_weather_map/resolver_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 require 'open_weather_map/resolver'
 
-# when called with a known city name, the assert function returns the correct id
-# when called with an unknown city name, the assert function returns nil
-
 RSpec.describe OpenWeatherMap::Resolver do
   describe '#city_id' do
     it 'returns the correct id for a known city' do
