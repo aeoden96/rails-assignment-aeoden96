3953f9ab7a2c15c1698db125a164ac99c7abfb8d
mateo <mateo1559@gmail.com>
2024-07-30 01:13:52 +0200

    feat: add flight includes to Booking

--- app/queries/bookings_query.rb
+++ app/queries/bookings_query.rb
@@ -4,7 +4,8 @@ class BookingsQuery
   end
 
   def call
-    bookings = Booking.all
+    bookings = Booking.includes(:flight)
+                      .order('flights.departs_at ASC, flights.name ASC, bookings.created_at ASC')
     bookings = bookings.with_active_flights if active_filter?
     bookings.order(:name)
   end
