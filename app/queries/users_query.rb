4adb55ef44b07a438d283279ef83fca6c1e5d395
mateo <mateo1559@gmail.com>
2024-08-01 12:12:56 +0200

    fix: user query bug

--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -7,7 +7,7 @@ class UsersQuery
     users = User.all
     users = users.order(:email)
     if @params[:query].present?
-      query = params[:query].downcase
+      query = @params[:query].downcase
       users = users.where(
         'LOWER(email) LIKE ? OR LOWER(first_name) LIKE ? OR LOWER(last_name) LIKE ?',
         "%#{query}%", "%#{query}%", "%#{query}%"
