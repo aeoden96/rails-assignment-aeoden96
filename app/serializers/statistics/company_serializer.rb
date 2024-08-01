940d62b17e2067b1c93bff674d3a63286ca96ffd
mateo <mateo1559@gmail.com>
2024-08-01 12:41:20 +0200

    fix:statistics

--- app/serializers/statistics/company_serializer.rb
+++ app/serializers/statistics/company_serializer.rb
@@ -5,7 +5,11 @@ module Statistics
     field :id, name: :company_id
 
     field :total_revenue do |company|
-      company.flights.sum { |flight| flight.bookings.sum(:no_of_seats) * flight.base_price }
+      company.flights.sum do |flight|
+        flight.bookings.sum do |booking|
+          booking.no_of_seats * booking.seat_price
+        end
+      end
     end
 
     field :total_no_of_booked_seats do |company|
@@ -15,7 +19,9 @@ module Statistics
     field :average_price_of_seats do |company|
       total_no_of_booked_seats = company.flights.sum { |flight| flight.bookings.sum(:no_of_seats) }
       total_revenue = company.flights.sum do |flight|
-        flight.bookings.sum(:no_of_seats) * flight.base_price
+        flight.bookings.sum do |booking|
+          booking.no_of_seats * booking.seat_price
+        end
       end
       total_revenue / total_no_of_booked_seats.to_f
     end
