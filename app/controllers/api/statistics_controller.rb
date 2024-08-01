56ef37c512ef27b9c9798ca32331b17616b82a56
mateo <mateo1559@gmail.com>
2024-08-01 12:26:43 +0200

    fix: authorize admin typo

--- app/controllers/api/statistics_controller.rb
+++ app/controllers/api/statistics_controller.rb
@@ -1,9 +1,7 @@
 module Api
   class StatisticsController < ApplicationController
     before_action :authenticate_user!
-    before_action :authenticate_admin!
-    # before_action :set_current_booking, only: [:update, :destroy, :show]
-    # before_action :authorize_action!, only: [:update, :destroy, :show]
+    before_action :authorize_admin!
 
     def flights_index
       flights = FlightsQuery.new(params).call
